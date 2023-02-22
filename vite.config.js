07c82af70989a979677dac7ebbcd8b334e4c079e
GWolf0 <aman.mohcine@gmail.com>
2023-02-22 12:40:45 +0100

    Replace dashes to underscores in app name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/frame-animation-previewer/',
+  base:'/frame_animation_previewer/',
   plugins: [react()],
 })
